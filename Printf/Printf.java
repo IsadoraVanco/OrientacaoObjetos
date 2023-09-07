41df1c2fe19da5cded751832fb6a92bc5eea8de3
Isadora <isadoravanco@gmail.com>
2023-09-07 14:18:41 -0300

    delete: exemplos básicos em Java

--- Printf/Printf.java
+++
@@ -1,6 +0,0 @@
-public class Printf {
-    public static void main(String[] args) {
-        //%n representa uma quebra de linha
-        System.out.printf("%s%n%s%n", "Welcome to", "Java Programming!");
-    }
-}
