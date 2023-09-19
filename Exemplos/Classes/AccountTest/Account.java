5580bce1f82a9f354c52728bb89a7b0be4d5b539
isadora <isadoravanco@gmail.com>
2023-09-19 17:05:17 -0300

    feat: Exemplos de Classe

---
+++ Exemplos/Classes/AccountTest/Account.java
@@ -0,0 +1,22 @@
+// exemplo de uma Conta de Banco
+public class Account {
+    // O atributo name é privado, ou seja, ele não pode ser modificado por outras classes
+    private String name; // instance variable
+
+    // constructor initializes name with parameter name
+    public Account(String name) { // constructor name is class name
+        this.name = name;
+    }
+        
+    //Nome de métodos: começam com um verbo e letra minúscula e as outras palavras em CamelCase
+
+    // method to set the name in the object
+    public void setName(String name) {
+        this.name = name; // store the name
+    }
+
+    // method to retrieve the name from the object
+    public String getName() {
+        return name; // return value of name to caller
+    }
+}
