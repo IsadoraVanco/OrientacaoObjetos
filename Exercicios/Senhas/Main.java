c173d2e8ed683251f387e4f7a1776e0e9377b22d
isadora <isadoravanco@gmail.com>
2023-10-12 21:31:32 -0300

    feat: Trabalho de Senhas

---
+++ Exercicios/Senhas/Main.java
@@ -0,0 +1,168 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main{
+
+    public static void limpaTela() {
+        try {
+            final String os = System.getProperty("os.name");
+            if (os.contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (Exception e) {
+            // Lidar com exceções, se houver, como IOException ou InterruptedException
+        }
+    }
+
+    public static void imprimeMenu(){
+        System.out.println("***********************************");
+        System.out.println("**     GERENCIADOR DE SENHAS     **");
+        System.out.println("**                               **");
+        System.out.println("**  Escolha uma opção:           **");
+        System.out.println("**                               **");
+        System.out.println("** [0] Emitir nova senha         **");
+        System.out.println("** [1] Retira senha da fila      **");
+        System.out.println("** [2] Atender senha             **");
+        System.out.println("** [3] Buscar senha na fila      **");
+        System.out.println("** [4] Imprimir fila             **");
+        System.out.println("** [5] Encerrar atendimento      **");
+        System.out.println("***********************************");
+
+    }
+
+    public static int leComando(){
+        int comando;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("**  Comando: ");
+        comando = scan.nextInt();
+
+        return comando;
+    }
+
+    public static int leSenha(){
+        int senha;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("**  Senha: ");
+        senha = scan.nextInt();
+
+        return senha;
+    }
+
+    public static boolean ehPreferencial(){
+        String resposta;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("**  É preferencial? (S/N): ");
+        resposta = scan.nextLine();
+
+        return Objects.equals(resposta, "S");
+    }
+
+    public static void main(String[] args){
+        //Número de atendimentos preferenciais sequenciais
+        int sequenciaPreferencial = 0;
+        int comando, senha;
+
+        Senhas filaComum = new Senhas();
+        Senhas filaPreferencial = new Senhas();
+        //Um ponteiro para a fila que será atendida
+        Senhas filaAtendimento;
+
+        do{
+            //Imprime o menu de opções
+            imprimeMenu();
+
+            //Lê o comando
+            comando = leComando();
+
+            switch (comando) {
+                case 0 -> {
+                    //Verifica se é fila preferencial
+                    if(ehPreferencial()){
+                        filaAtendimento = filaPreferencial;
+                    }else{
+                        filaAtendimento = filaComum;
+                    }
+
+                    //Emite nova senha
+                    filaAtendimento.emitir();
+                }case 1 -> {
+                    //Verifica se é fila preferencial
+                    if(ehPreferencial()){
+                        filaAtendimento = filaPreferencial;
+                    }else{
+                        filaAtendimento = filaComum;
+                    }
+
+                    //Lê a senha a ser retirada
+                    senha = leSenha();
+
+                    //Desiste da fila
+                    filaAtendimento.apagar(senha);
+                }case 2 -> {
+                    if(filaComum.retornaTamanho() == 0 && filaPreferencial.retornaTamanho() == 0){
+                        System.out.println(">> Não há filas para atender");
+                    }else{
+                        //Verifica se vai atender a fila preferencial
+                        if ((sequenciaPreferencial < 2 && filaPreferencial.retornaTamanho() > 0)
+                                || (filaComum.retornaTamanho() == 0)) {
+                            System.out.println(">> Fila preferencial sendo atendida");
+                            filaAtendimento = filaPreferencial;
+                            sequenciaPreferencial++;
+                        } else {
+                            System.out.println(">> Fila comum sendo atendida");
+                            sequenciaPreferencial = 0;
+                            filaAtendimento = filaComum;
+                        }
+
+                        //Atende a primeira senha da fila
+                        filaAtendimento.atender();
+                    }
+                }case 3 -> {
+                    //Verifica se é fila preferencial
+                    if(ehPreferencial()){
+                        filaAtendimento = filaPreferencial;
+                    }else{
+                        filaAtendimento = filaComum;
+                    }
+
+                    //Lê a senha a ser retirada
+                    senha = leSenha();
+
+                    //Busca a posição que a senha está
+                    filaAtendimento.busca(senha);
+                }
+                case 4 -> {
+                    //Verifica se é fila preferencial
+                    if(ehPreferencial()){
+                        filaAtendimento = filaPreferencial;
+                    }else{
+                        filaAtendimento = filaComum;
+                    }
+
+                    //Imprime a fila
+                    filaAtendimento.imprime();
+                }
+                case 5 -> {
+                    //Mostra o total de senhas atendidas
+                    System.out.printf(">> Total de senhas atendidas na preferencial: %d%n", filaPreferencial.retornaAtendidas());
+                    System.out.printf(">> Total de senhas atendidas na comum: %d%n%n", filaComum.retornaAtendidas());
+
+                    //Encerra o sistema
+                    System.out.println(">> Obrigada por utilizar o nosso sistema! Até a próxima :)");
+                }
+                default -> {
+                    System.out.println(">> Comando inválido. Digite novamente!");
+                }
+            }
+
+        }while(comando != 5);
+
+    }
+}
---
+++ Exercicios/Senhas/Senhas.java
@@ -0,0 +1,103 @@
+import java.util.ArrayList;
+import java.security.SecureRandom;
+
+public class Senhas{
+    //Fila de senhas
+    private ArrayList<Integer> fila;
+    //Número de senhas atendidas no total
+    private int senhasAtendidas = 0;
+
+    public Senhas(){
+        this.fila = new ArrayList<Integer>();
+    }
+
+    public void emitir(){
+        SecureRandom sorteador = new SecureRandom();
+
+        //Sorteia uma senha
+        int senha = sorteador.nextInt(0, 999);
+
+        //Verifica se já existe na fila e sorteia denovo
+        while(this.fila.contains(senha)){
+            senha = sorteador.nextInt(0, 999);
+        }
+
+        //Adiciona na fila
+        this.fila.add(senha);
+
+        //Mostra a senha gerada
+        System.out.printf("=> A nova senha é: %s%n", String.format("%03d", senha));
+    }
+
+    public void apagar(int senha){
+        if(this.fila.isEmpty()){
+            System.out.println("!!  A fila está vazia");
+            return;
+        }else if(!this.fila.contains(senha)){
+            System.out.println("!! A senha não está na fila");
+            return;
+        }
+
+        //Procura a senha na fila e apaga
+        this.fila.remove((Integer) senha);
+        System.out.println("=> Senha Removida com sucesso!");
+    }
+
+    public void atender(){
+        if(this.fila.isEmpty()){
+            System.out.println("!! A fila está vazia");
+            return;
+        }
+
+        //Primeira senha da fila
+        int senha = this.fila.get(0);
+
+        System.out.printf("=> Senha a ser atendida: %s%n", String.format("%03d", senha));
+
+        //Atende (retira) da fila a primeira senha
+        this.fila.remove(0);
+        senhasAtendidas++;
+    }
+
+    public void busca(int senha){
+        if(this.fila.isEmpty()){
+            System.out.println("!! A fila está vazia");
+            return;
+        }
+
+        //Procura pela senha na fila
+        int posicao = this.fila.indexOf((Integer) senha);
+
+        if(posicao < 0){
+            System.out.println("!! A senha não está na fila");
+            return;
+        }
+
+        //Imprime a posição da senha na fila
+        System.out.printf("=> A senha solicitada está na %da posição%n", posicao + 1);
+    }
+
+    public void imprime(){
+        if(this.fila.isEmpty()){
+            System.out.println("!! A fila está vazia ");
+            return;
+        }
+
+        //Imprime toda a fila de senhas
+        System.out.println("=> Fila completa:");
+        for(Integer i : this.fila){
+            System.out.printf("%s ", String.format("%03d", i));
+        }
+        System.out.println();
+    }
+
+    public int retornaTamanho(){
+        //Retorna o tamanho da fila
+        return this.fila.size();
+    }
+
+    public int retornaAtendidas(){
+        //Retorna o número de senhas atendidas na fila
+        return this.senhasAtendidas;
+    }
+}
