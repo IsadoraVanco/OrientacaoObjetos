11e77951edbddc1e7b79fd0008553f916d576ad2
isadora <isadoravanco@gmail.com>
2023-11-16 01:09:38 -0300

    feat: Interface Grafica com Swing

---
+++ Exemplos/InterfaceGrafica/BotaoSimples.java
@@ -0,0 +1,32 @@
+package Exemplos.InterfaceGrafica;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class BotaoSimples {
+    public static void main(String[] args) {
+        // Cria um novo JFrame
+        JFrame frame = new JFrame("Exemplo");
+            frame.setSize(300, 200);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Cria um novo JButton
+        JButton button = new JButton("Clique em mim");
+
+        // Adiciona um ActionListener ao botão
+            button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Este código será executado quando o botão for clicado
+                System.out.println("Botão clicado!");
+            }
+        });
+
+        // Adiciona o botão ao JFrame
+            frame.getContentPane().add(button);
+
+        // Torna o JFrame visível
+            frame.setVisible(true);
+    }
+}
